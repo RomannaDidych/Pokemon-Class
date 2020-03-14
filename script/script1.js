e7d85724982064ce4afde8dfac5f57348f0ea2c5
unknown <romanna.d@gmail.com>
2020-03-14 22:03:25 +0200

    got all information from server (3 steps)

--- script/script1.js
+++ script/script1.js
@@ -1,6 +1,9 @@
 let nextPageURL = '';
 let arrPokemons = [];
-const step = 20;
+let tempArrPokemons = [];
+let tempAbilNames = [];
+
+let step = 0;
 let index = 0; 
 let numberOnPage = 0;
 
@@ -25,6 +28,22 @@ class Pokemon{
 		this.url = url
 		this.abilNumber = 0
 	}
+	//Pokemon methods
+	setAbilitiesNumber(number){
+		this.abilNumber = number;
+	}
+
+	getAbilitiesNumber(){
+		return this.abilNumber;
+	}
+
+	setAbilities(array){
+		this.abilities = array;
+	}
+
+	getAbilities(){
+		return this.abilities;
+	}
 }
 
 function getAllData(url){
@@ -33,11 +52,12 @@ function getAllData(url){
 	.then(function(value){
 		nextPageURL = value.next;
 		let temp = value.results;
+		step = temp.length;
 		let arrNames = temp.map(temp => temp.name);				
-		let arrUrls = temp.map(temp => temp.url);
+		let arrUrls = temp.map(temp => temp.url);		
 		for(let i=0+index; i<temp.length; i++ ){
 			let pokemon = new Pokemon(arrNames[i], arrUrls[i]);			
-			arrPokemons.push(pokemon);
+			tempArrPokemons.push(pokemon);
 		};
 
 		let arrPromises = [];
@@ -49,35 +69,44 @@ function getAllData(url){
 	.then(function(arr){
 		//console.log(arr);
 		let arrAbilities = arr.map(arr => arr.abilities);
-		console.log(arrAbilities);
+		//console.log(arrAbilities);
+		let tempAbilUrls = [];
 		for (let j=0; j<arrAbilities.length; j++){
 			let tempArr = arrAbilities[j];
+			tempArrPokemons[j].setAbilitiesNumber(tempArr.length);
+			console.log(arrPokemons[j].getAbilitiesNumber()); 
 			let tempAbilArr = tempArr.map(tempArr => tempArr.ability);
 			//console.log(tempAbilArr);
-			let names = tempAbilArr.map(tempAbilArr => tempAbilArr.name);
-			let urls = tempAbilArr.map(tempAbilArr => tempAbilArr.url);
-			console.log(names);
-			console.log(urls);
-
+			//let names = tempAbilArr.map(tempAbilArr => tempAbilArr.name);
+			tempAbilNames = tempAbilNames.concat(tempAbilArr.map(tempAbilArr => tempAbilArr.name));
+			//let urls = tempAbilArr.map(tempAbilArr => tempAbilArr.url);
+			tempAbilUrls = tempAbilUrls.concat(tempAbilArr.map(tempAbilArr => tempAbilArr.url));
+			//console.log(names);
+			//console.log(urls);
 		};
-		
-
-
-	/*async function processArr(arr){
-			let k = -1;
-			let valueArr = [];
-			for(item of arr){
-				k += 1;
-				valueArr[k] = await getServersData(item);
-			}
-			return valueArr;
-		}
-
-		for (let i= 0+index; i<arrAbilities.length; i++){
-
-		}*/
-		
-
+		console.log(tempAbilNames);
+		console.log(tempAbilUrls);
+		let arrPromises = [];
+		for(let i=0; i<tempAbilUrls.length; i++){
+		 	arrPromises.push(getServersData(tempAbilUrls[i]));		
+		};		
+		return Promise.all(arrPromises);
+	})
+	.then(function(data){
+		let arrEffects = data.map(data => data.effect_entries[0]["effect"]);
+		console.log(arrEffects);
+		/*for( let i=0; i<step; i++){
+			let abilities = [];
+			let numberAbilities = tempArrPokemons[i].getAbilitiesNumber();
+			for(j=0; j<numberAbilities; j++){
+				let ability = [tempAbilNames[i+j], arrEffects[i+j]];
+				abilities.push(ability); 
+			};
+			console.log(abilities);
+			tempArrPokemons[i].setAbilities(abilities);
+			console.log("Pokemons " + arrPokemons[i].name + " abilities: ");
+			console.log(arrPokemons[i].getAbilities());
+		};*/
 	});
 };	
 
